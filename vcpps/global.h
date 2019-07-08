19fc5ec56a49166f1df3f2eaa197d1e5869217b5
Du Yanning <duyanning@gmail.com>
2019-07-08 23:16:25 +0800

    genesis

---
+++ vcpps/global.h
@@ -0,0 +1,10 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+extern string gcc_compile_cpp_cmd;
+extern string gcc_compile_h_cmd;
+extern string gcc_link_cmd;
+extern string extra_compile_flags;
+extern string extra_link_flags;
+
+#endif // GLOBAL_H
---
+++ vcpps/helpers.h
@@ -0,0 +1,12 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+fs::path get_home();
+fs::path shadow(fs::path p);
+bool can_write_in(fs::path dir);
+void move(const fs::path& old_p, const fs::path& new_p);
+bool is_a_cpp_src(fs::path f);
+bool is_a_c_src(fs::path f);
+void safe_remove(fs::path f);
+
+#endif // HELPERS_H
